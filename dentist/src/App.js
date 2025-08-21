6e2a44ea7149bb5a774a98611a3d489db61bad03
Miraz92 <grmiraz2.0@gmail.com>
2025-08-21 23:17:40 +0600

    Home, Service,Reviews, Contact

--- dentist/src/App.js
+++ dentist/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+
+// Pages
+import Home from "./pages/Home";
+import Reviews from "./pages/Reviews";
+import Contact from "./pages/Contact";
+import ServiceBooking from "./components/ServiceBooking";
+import DoctorsList from "./components/DoctorsList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="d-flex">
+        <Sidebar />
+        <div className="flex-grow-1 bg-light min-vh-100 p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<ServiceBooking />} />
+            <Route path="/doctors" element={<DoctorsList />} />
+            <Route path="/reviews" element={<Reviews />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
